570d5c97d1b894a171debf6c1726c74024610d1e
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-22 20:11:38 +0000

    Finalized all outcomes

--- main.cs
+++ main.cs
@@ -21,19 +21,37 @@ class Program {
           Console.WriteLine("Invalid input, please enter a temperature between -30 and 130");
           i--;
         }
-    }
-     Console.Write("5-day temperature [");
+    }// end of user input loop
+
+
+
+    if ( dailyTemp[0] > dailyTemp[1] && dailyTemp[1] > dailyTemp[2] &&               dailyTemp[2] > dailyTemp[3] && dailyTemp[3] > dailyTemp[4])
+        {
+          Console.WriteLine(outputs[0]);
+        }
+    else if( dailyTemp[0] < dailyTemp[1] && dailyTemp[1] < dailyTemp[2] &&           dailyTemp[2] < dailyTemp[3] && dailyTemp[3] < dailyTemp[4])
+        {
+         Console.WriteLine(outputs[1]);
+        }   
+    else {
+          Console.WriteLine(outputs[2]);
+        }
+ 
+    
+    // above  statement checks if the temperatures are in order from coldest to hottest, hottest to coldest or neither
+    
+     Console.Write("5-day temperature ["); // displays the 5 day temperatures
  foreach(double temp in dailyTemp)
     {
         Console.Write(temp+", ");
     }
      Console.WriteLine("]");
     
-   double total = 0;
+   double total = 0; // creates a value to store all the days temp added together
     for(int k = 0; k < 5; k++)
        { 
            total  = total + (dailyTemp[k]);
       }
-    Console.WriteLine((total/5));
+    Console.WriteLine("Average temperature is "+(total/5)+ " degrees");// displays the average weekly temperature
  }
 }
