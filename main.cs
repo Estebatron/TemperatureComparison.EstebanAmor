3040bca27d41e190797ecbf490a1a9567f046840
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-22 02:32:54 +0000

    Created a couple for loops and foreach loops

--- main.cs
+++ main.cs
@@ -3,24 +3,37 @@ using System;
 class Program {
   public static void Main (string[] args) {
     
-int day1 = 0;
-int day2 = 0;
-int day3 = 0;
-int day4 = 0;
-int day5 = 0;
+    double day1 = 0;
+    double day2 = 0;
+    double day3 = 0;
+    double day4 = 0;
+    double day5 = 0;
     
-    int[] tempArr = {day1,day2,day3,day4,day5};
-    String[] outputs = {"Its getting colder", "Its getting warmer", "Its a mixed bag"};
-  for(int i = 0; i < 5; i++)
-      {
+  double[] dailyTemp = {day1,day2,day3,day4,day5};
+  String[] outputs = {"Its getting colder", "Its getting warmer", "Its a mixed bag"};
+    
+ for(int i = 0; i < 5; i++)
+    {
       Console.WriteLine ("Input temperature for day " + (i+1) );
-        tempArr[i] = int.Parse(Console.ReadLine());
-      if( tempArr[i] >130 || tempArr[i] < -30)
+        dailyTemp[i] = int.Parse(Console.ReadLine());
+      if( dailyTemp[i] >130 || dailyTemp[i] < -30)
         {
-          Console.WriteLine("Invalid input");
+          Console.WriteLine("Invalid input, please enter a temperature between -30 and 130");
           i--;
         }
+    }
+     Console.Write("5-day temperature [");
+ foreach(double temp in dailyTemp)
+    {
+        Console.Write(temp+", ");
+    }
+     Console.WriteLine("]");
+    
+   double total = 0;
+    for(int k = 0; k < 5; k++)
+       { 
+           total  = total + (dailyTemp[k]);
       }
-    Console.WriteLine(tempArr[4]);
-  }
+    Console.WriteLine((total/5));
+ }
 }
